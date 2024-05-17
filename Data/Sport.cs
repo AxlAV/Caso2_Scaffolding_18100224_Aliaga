4f2a2d94e1168bbd302f9ae8d89287b5584db56f
laboratorioesan <laboratorioesan@WS2302499>
2024-05-17 16:25:35 -0500

    Agregar archivos de proyecto.

---
+++ Data/Sport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParcialWEBCaso1.Data;
+
+public partial class Sport
+{
+    public int? Id { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Country { get; set; }
+
+    public DateOnly? FoundationDate { get; set; }
+}
---
+++ Data/Team.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParcialWEBCaso1.Data;
+
+public partial class Team
+{
+    public int? Id { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Country { get; set; }
+
+    public int? NumberChampions { get; set; }
+}
